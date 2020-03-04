42fc77f8170e0fb1e35a8534ea25a1b511734a40
João Paulo <jpsouza.rod@gmail.com>
2020-03-04 11:34:13 -0300

    Create n_primos.py

---
+++ n_primos.py
@@ -0,0 +1,14 @@
+op="S"
+while op=="S" or op=="s":
+    numero = int(input("Digite um valor: "))
+    cont=0
+    aux = 1
+    while aux<=numero:
+        if numero%aux==0:
+            cont+=1
+        aux+=1
+    if cont>2:
+        print("valor nao é primo")
+    else:
+        print("valor primo")
+    op=str(input("Desejas continuar(S ou N): "))
