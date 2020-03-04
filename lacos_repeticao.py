dda76a17514272c56c3833f08f33b2f9542d1031
João Paulo <jpsouza.rod@gmail.com>
2020-03-04 10:10:16 -0300

    Update lacos_repeticao.py

--- lacos_repeticao.py
+++ lacos_repeticao.py
@@ -1,1 +1,13 @@
+num = 1000
+cont=0
+i=0
+
+while num>=1000 and num<=2000:
+    if num%2 == 0:
+        cont+=1
+    num+=1
+print ("O numero de numeros pares é ", cont)
+
+for i in range(10): #for i=0; i<n; i++
+    print("n :", i+1)
 
